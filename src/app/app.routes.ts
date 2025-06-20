3e150e917d410b27d04a512b6eeb4872481263c2
EnzoCruz0289 <josedavidaragoncruz@gmail.com>
2025-06-19 23:38:26 -0500

    Primer commit del proyecto local en rama desarrollo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ConsultationComponent } from './pagges/consultation/consultation.component';
+import { DatasedComponent } from './pagges/datased/datased.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'consulta', component:ConsultationComponent},
+    {path:'data', component:DatasedComponent}
+];
---
+++ src/app/pagges/datased/datased.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-datased',
+  standalone: true,
+  imports: [],
+  templateUrl: './datased.component.html',
+  styleUrl: './datased.component.css'
+})
+export class DatasedComponent {
+
+}
